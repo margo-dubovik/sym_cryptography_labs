43366561b651d6e624d77d6253aa89386cef3e7b
MarharytaDubovyk <margodubovik531@gmail.com>
2021-03-12 00:11:00 +0200

    key FOUND

--- coincidence_statistics.py
+++ coincidence_statistics.py
@@ -59,8 +59,40 @@ def num_to_letter(y):
         word.append(ru_alph[item])
     return word
 
+def M_i(n, g):  #function M_i(g)
+    m_i = 0
+    for i in range (r):
+        for t in range(len(ru_alph)):
+            let = ru_alph[t]
+            z = freqs[n][(t+g) % m]
+            m_i = m_i + ru_freqs.get(let) * z
+    return m_i
+
+def find_k_n(n): #meaning of n_th letter of the key
+    m_values = []
+    for g in range (m):
+        m_i = M_i(n, g)
+        m_values.append(m_i)
+    k_n = m_values.index(max(m_values))
+    return k_n
+
+def find_key():
+    k = []
+    for j in range(r):
+        k.append(find_k_n(j))
+    return k
+
 ru_alph = ['а', 'б', 'в', 'г', 'д', 'е', 'ж', 'з', 'и', 'й','к', 'л', 'м', 'н', 'о', 'п',
            'р', 'с', 'т', 'у', 'ф', 'х', 'ц', 'ч', 'ш', 'щ', 'ъ','ы', 'ь', 'э', 'ю', 'я']
+
+ru_freqs = {'а': 0.08413309204466018, 'б': 0.018444537359456968, 'в': 0.040462673208464264, 'г': 0.01711178764854606,
+            'д': 0.02942828957254013, 'е': 0.07911241482488061,  'ж': 0.009113656111376064, 'з': 0.017559304095592126,
+            'и': 0.06170010518269777, 'й': 0.011478633539561108, 'к': 0.03969340223562231, 'л': 0.05009897626529559,
+            'м': 0.034354237033455935, 'н': 0.06501238019952048, 'о': 0.1144482481527697, 'п': 0.028178836718561154,
+            'р': 0.04283091718005841, 'с': 0.05246558696518518, 'т': 0.062276650094403106, 'у': 0.030447451116177884,
+            'ф': 0.0015924399119339897, 'х': 0.009015659809103203, 'ц': 0.0038561544944370764, 'ч': 0.015486682302521117,
+            'ш': 0.008515878667511613, 'щ': 0.002889257645344849, 'ъ': 0.00022865803863667545, 'ы': 0.019801786145936095,
+            'ь': 0.018248544754911247, 'э': 0.0031946794540952655, 'ю': 0.00484755041909752, 'я': 0.023942129916964466}
 
 
 d = find_r(text)
@@ -73,13 +105,20 @@ freqs = letter_freqs(Y) #freqs of letters of block
 most_freq = find_most_freq(freqs) #indexes of most frequent letters of the block
 print("max_let_ind=",most_freq)
 
-#визначення символів ключа прирівнюючи найчастіші літери в мові
+### визначення символів ключа прирівнюючи найчастіші літери в мові
 # "o" is most frequent in russian. position:14
 m = 32
 
-key_numbers = find_key_letter(most_freq)
-print(key_numbers) #key letters`numbers
+key_numbers_1 = find_key_letter(most_freq) #key letters`numbers
+print("key 1:\n", key_numbers_1)
 
+key_word_1 = num_to_letter(key_numbers_1)
+print(key_word_1)
 
-key = num_to_letter(key_numbers)
-print(key)
+### визначення символів ключа за допомогою функції М_і(g)
+
+key_numbers_2 = find_key() #key letters`numbers
+print("key 2:\n", key_numbers_2)
+
+key_word_2 = num_to_letter(key_numbers_2)
+print(key_word_2)
